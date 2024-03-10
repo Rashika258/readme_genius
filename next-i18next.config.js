710d359c73b01c16f730412caeca5cc79b6399e4
Rashika258 <rashikasuresh5758@gmail.com>
2024-03-10 09:43:15 +0530

    i18next changes

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,6 @@
-module.exports = {
-  i18n: {
+ 
+ export const i18n= {
     defaultLocale: "en",
     locales: ["en", "tl"],
-  },
-};
+  }
+export default i18n
--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
-// const { i18n } = require("./next-i18next.config");
-// const { i18n } = require('./next-i18next.config')
 
-import config from './next-i18next.config.js';
-const { i18n } = config;
 
-// import {i18n} from "./next-i18next.config"
+import i18n from "./next-i18next.config";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
